102ee890bd73c585e5978de02a92f811df476f6b
Aztec <salgorog@hotmail.com>
2020-11-01 18:04:13 -0700

    fixed edit, and delete post bug

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,19 +1,26 @@
 async function editFormHandler(event) {
     event.preventDefault();
-
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
+  
     const title = document.querySelector('input[name="post-title"]').value.trim();
-
-    const response = await fetch(`/api/posts/:${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({ title }),
-        headers: { 'Content-Type': 'application/json' }
-    })
-
-    if (response.ok) document.location.replace('/dashboard');
-    else alert(response.statusText);
-}
-
-document.querySelector('.comment-form').addEventListener('submit', editFormHandler);
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        title
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+  
